34206c4b5d5c2e06e369dc86732cfcc60929f182
Flávio Santos Alves <flavio.santos.pro@gmail.com>
2025-11-27 17:32:18 -0300

    feat: using css class with modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Heading } from './components/heading/Heading';
 
 function App() {
   console.log('opa');
 
   return (
     <>
+      <Heading />
       <h1>Opa</h1>
       <h1>Opa</h1>
       <h1>Opa</h1>
---
+++ src/components/heading/Heading.tsx
@@ -0,0 +1,20 @@
+import styles from './Heading.module.css';
+
+export function Heading() {
+  console.log(styles);
+  const classes = 'heading';
+  return (
+    <>
+      <h1 className={classes}>Titulo - classes</h1>;{' '}
+      {/** nao funciona pois o nome da classe agora é unico  */}
+      <h1 className='heading'>Titulo - heading</h1>;{' '}
+      {/** nao funciona pois o nome da classe agora é unico  */}
+      <h1 className={styles.heading}>Titulo - styles.purble</h1>;
+      {/** pega direto do modules css */}
+      <h1 className={`${styles.heading} ${styles.purble}`}>
+        Titulo - styles.purble
+      </h1>
+      ;{/** mais de uma classe */}
+    </>
+  );
+}
