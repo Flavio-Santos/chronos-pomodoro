43ed93408f557237a463ce3c02cfc01a3c184ae6
Flávio Santos Alves <flavio.santos.pro@gmail.com>
2025-11-27 23:09:19 -0300

    feat(aula-20): site sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Heading } from './components/heading/Heading';
-import { TimerIcon } from 'lucide-react';
 
 function App() {
-  console.log('opa');
-
   return (
     <>
-      <Heading>
-        Ola mundo
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt iusto
-        neque libero dolores suscipit ullam odit, optio animi dolorem amet autem
-        a repellendus ex sit repellat cupiditate quae voluptas officia?
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
