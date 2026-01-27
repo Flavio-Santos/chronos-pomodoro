b5d11db436ea8928b94c49265437deba916eea00
Flávio Santos Alves <flavio.santos.pro@gmail.com>
2026-01-27 15:15:54 -0300

    feat(aula-26): Init Form

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,30 @@ function App() {
       <Container>
         <CountDown />
       </Container>
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='inputTask'>task</label>
+            <input type='text' id='inputTask' />
+          </div>
+
+          <div className='formRow'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+              quod.
+            </p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0000</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Click</button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
